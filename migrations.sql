0fc076f1d16266488e601808909f6411711502cd
Zhambal <zhambikayurov09@gmail.com>
2024-11-13 12:34:32 +0800

    Update 2024-11-13

--- migrations.sql
+++ migrations.sql
@@ -1,4 +1,4 @@
--- Active: 1731418800391@@127.0.0.1@5432@postgres
+
 CREATE TABLE teams (
     team_id SERIAL PRIMARY KEY ,
     name VARCHAR(64) NOT NULL CHECK (name <> '' AND name NOT LIKE '% ' AND name NOT LIKE ' %'),
@@ -49,47 +49,49 @@ CREATE TABLE coaches (
     name VARCHAR(64) NOT NULL CHECK (name <> '' AND name NOT LIKE '% ' AND name NOT LIKE ' %'),
     surname VARCHAR(64) NOT NULL CHECK (surname <> '' AND surname NOT LIKE '% ' AND surname NOT LIKE ' %'),
     country VARCHAR(64) NOT NULL CHECK (country <> '' AND country NOT LIKE '% ' AND country NOT LIKE ' %'),
-    job_title VARCHAR(64) NOT NULL CHECK (job_title <> '' AND job_title NOT LIKE '% ' AND job_title NOT LIKE ' %'),
     UNIQUE (name, surname)  
 );
 
-INSERT INTO coaches (name, surname, country, job_title)
+INSERT INTO coaches (name, surname, country)
 VALUES
-('Pep', 'Guardiola', 'Spain', 'Head Coach'),
-('Mikel', 'Arteta', 'Spain', 'Head Coach'),
-('Juanma', 'Lillo', 'Spain', 'Assistant Coach'),
-('Jürgen', 'Klopp', 'Germany', 'Head Coach'),
-('Gerardo', 'Martino', 'Argentina', 'Head Coach'),
-('Vincent', 'Kompany', 'Belgium', 'Head Coach'),
-('Hansi', 'Flik', 'Germany', 'Head Coach'),
-('Carlo', 'Ancelotti', 'Italy', 'Head Coach'),
-('Unai', 'Emery', 'Spain', 'Head Coach'),
-('Luciano', 'Spalletti', 'Italy', 'Head Coach');
+('Pep', 'Guardiola', 'Spain'),
+('Mikel', 'Arteta', 'Spain'),
+('Juanma', 'Lillo', 'Spain'),
+('Jürgen', 'Klopp', 'Germany'),
+('Gerardo', 'Martino', 'Argentina'),
+('Vincent', 'Kompany', 'Belgium'),
+('Hansi', 'Flik', 'Germany'),
+('Carlo', 'Ancelotti', 'Italy'),
+('Unai', 'Emery', 'Spain'),
+('Luciano', 'Spalletti', 'Italy');
 
 CREATE TABLE team_coaches (
-    team_id INT,
-    coach_id INT,
-    PRIMARY KEY (team_id, coach_id),
+    team_coaches_id SERIAL PRIMARY KEY ,
+    team_id INT NOT NULL,
+    coach_id INT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    job_title VARCHAR(64) NOT NULL CHECK (job_title <> '' AND job_title NOT LIKE '% ' AND job_title NOT LIKE ' %'),
     FOREIGN KEY (team_id) REFERENCES teams(team_id),
     FOREIGN KEY (coach_id) REFERENCES coaches(coach_id)
 );
 
-INSERT INTO team_coaches (team_id, coach_id)
+INSERT INTO team_coaches (team_id, coach_id,start_date, end_date, job_title)
 VALUES
-(1, 1),
-(2, 2),
-(1, 3),
-(4, 4),
-(5, 5),
-(6, 6),
-(7, 7),
-(8, 8),
-(9, 9),
-(10, 10);
+(7, 7, '2024-08-26','2024-08-26', 'Head Coach'),
+(8, 8, '2013-11-27', '2024-08-26','Head Coach'),
+(1, 3, '2023-06-12', '2024-08-26','Head Coach'),
+(4, 4, '2016-01-07', '2024-08-26','Assistant'),
+(8, 8, '2021-07-30', '2024-08-26','Head Coach'),
+(6, 6, '2024-10-26', '2024-08-26','Head Coach'),
+(7, 7, '2024-10-26', '2024-08-26','Head Coach'),
+(8, 8, '2024-10-26', '2024-08-26','Head Coach'),
+(9, 9, '2024-10-26', '2024-08-26','Head Coach'),
+(10, 10,'2024-10-26','2024-08-26','Head Coach');
 
 
 CREATE TABLE matches (
-    match_id SERIAL PRIMARY KEY ,
+    match_id SERIAL PRIMARY KEY,
     team_1_id INT NOT NULL,
     team_2_id INT NOT NULL,
     team_1_goals INT DEFAULT 0 CHECK (team_1_goals >= 0),
